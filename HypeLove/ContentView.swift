5168183daa9e9468716d5fc2289e13c9597ad831
Jaume Corbi <jaume4@gmail.com>
2020-08-06 10:22:20 +0200

    Initial Commit

---
+++ HypeLove/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HypeLove
+//
+//  Created by Jaume on 06/08/2020.
+//  Copyright © 2020 Jaume. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
