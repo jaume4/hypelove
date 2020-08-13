82058c6b05a4d341d7c8780a310139c10e991af8
Jaume Corbi <jaume4@gmail.com>
2020-08-13 09:52:21 +0200

    loading button

--- Shared/Modifiers/HypeButtons.swift
+++ Shared/Modifiers/HypeButtons.swift
@@ -9,7 +9,52 @@ import Foundation
 import SwiftUI
 
 struct HypeButton: ButtonStyle {
-    var enabled: Bool = true
+    var enabled = true
+    var loading = false
+    
+    func fillColor(isPressed: Bool, enabled: Bool) -> Color {
+        switch (isPressed, enabled) {
+        case (_, false): return Color.buttonDisabled
+        case (true, true): return Color.buttonMain.opacity(0.7)
+        case (false, true): return Color.buttonMain
+        }
+    }
+    
+    func backgroundView(for configuration: ButtonStyleConfiguration) -> some View {
+        RoundedRectangle(cornerRadius: 10, style: .continuous)
+            .fill(fillColor(isPressed: configuration.isPressed, enabled: enabled))
+    }
+    
+    @ViewBuilder
+    func overlay(for configuration: ButtonStyleConfiguration) -> some View {
+        if loading {
+            ZStack {
+                backgroundView(for: configuration)
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            }
+        }
+        else {
+            EmptyView()
+        }
+    }
+    
+    func makeBody(configuration: ButtonStyleConfiguration) -> some View {
+        configuration.label
+            .font(Font.body.bold())
+            .frame(maxWidth: .infinity)
+            .foregroundColor(Color.white.opacity(configuration.isPressed ? 0.7 : 1.0))
+            .padding(8)
+            .background(backgroundView(for: configuration))
+            .overlay(overlay(for: configuration))
+    }
+}
+
+//MARK: - Secondary Button
+
+struct HypeSecondaryButton: ButtonStyle {
+    var enabled = true
+    var expand = true
     var loading = false
     
     func fillColor(isPressed: Bool, enabled: Bool) -> Color {
@@ -21,46 +66,23 @@ struct HypeButton: ButtonStyle {
     }
     
     @ViewBuilder
-    func backgroundView(configuration: ButtonStyleConfiguration) -> some View {
-        if loading {
-            AnimatableGradient(startColor: .buttonMain, endColor: .buttonDisabled)
-                .cornerRadius(10)
-        }
-        else {
-            RoundedRectangle(cornerRadius: 10, style: .continuous)
-                .fill(fillColor(isPressed: configuration.isPressed, enabled: enabled))
-        }
-    }
-    
     func makeBody(configuration: ButtonStyleConfiguration) -> some View {
-        configuration.label
-            .font(Font.body.bold())
-            .frame(maxWidth: .infinity)
-            .foregroundColor(Color.white.opacity(configuration.isPressed ? 0.7 : 1.0))
-            .padding(8)
-            .background(backgroundView(configuration: configuration))
-    }
-}
-
-struct HypeSecondaryButton: ButtonStyle {
-    var enabled: Bool = true
-    var expand: Bool = true
-    
-    func fillColor(isPressed: Bool, enabled: Bool) -> Color {
-        switch (isPressed, enabled) {
-        case (_, false): return Color.buttonDisabled
-        case (true, true): return Color.buttonMain.opacity(0.7)
-        case (false, true): return Color.buttonMain
+        if !loading {
+            configuration.label
+                .frame(maxWidth: expand ? .infinity : nil)
+                .font(Font.body.bold())
+                .foregroundColor(fillColor(isPressed: configuration.isPressed, enabled: enabled))
+                .padding([.leading, .trailing], expand ? 16 : 0)
+                .padding([.top, .bottom], 8)
+        } else {
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: .buttonMain))
+                .frame(maxWidth: expand ? .infinity : nil)
+                .font(Font.body.bold())
+                .foregroundColor(fillColor(isPressed: configuration.isPressed, enabled: enabled))
+                .padding([.leading, .trailing], expand ? 16 : 0)
+                .padding([.top, .bottom], 8)
         }
-    }
-    
-    func makeBody(configuration: ButtonStyleConfiguration) -> some View {
-        configuration.label
-            .frame(maxWidth: expand ? .infinity : nil)
-            .font(Font.body.bold())
-            .foregroundColor(fillColor(isPressed: configuration.isPressed, enabled: enabled))
-            .padding([.leading, .trailing], expand ? 16 : 0)
-            .padding([.top, .bottom], 8)
     }
 }
 
@@ -78,8 +100,11 @@ struct HypeButtons_Previews: PreviewProvider {
                 .buttonStyle(HypeButton(enabled: false))
             Button("Secondary dis", action: {})
                 .buttonStyle((HypeSecondaryButton(enabled: false)))
-            Button("Primary", action: {})
+            Spacer().frame(height: 30)
+            Button("Loading", action: {})
                 .buttonStyle(HypeButton(loading: true))
+            Button("Loading", action: {})
+                .buttonStyle(HypeSecondaryButton(loading: true))
         }
         .padding()
         
@@ -93,8 +118,11 @@ struct HypeButtons_Previews: PreviewProvider {
                 .buttonStyle(HypeButton(enabled: false))
             Button("Secondary dis", action: {})
                 .buttonStyle((HypeSecondaryButton(enabled: false)))
-            Button("Primary", action: {})
+            Spacer().frame(height: 30)
+            Button("Loading", action: {})
                 .buttonStyle(HypeButton(loading: true))
+            Button("Loading", action: {})
+                .buttonStyle(HypeSecondaryButton(loading: true))
         }
         .padding()
         .preferredColorScheme(.dark)
